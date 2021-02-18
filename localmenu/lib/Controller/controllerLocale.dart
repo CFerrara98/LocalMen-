a1cb44d382c7b63912a6b39051075dc4a1ffa739
ldesantis123 <63883615+ldesantis123@users.noreply.github.com>
2021-02-18 17:56:06 +0100

    controller update

---
+++ localmenu/lib/Controller/controllerLocale.dart
@@ -0,0 +1,55 @@
+
+import '../Beans/Locale.dart';
+import '../Beans/Locale.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geolocator/geolocator.dart';
+
+class ControllerLocale{
+
+  // Init firestore and geoFlutterFire
+
+  static Future<List<LocalePreview>> _initLocaliFromCategory(String Category) async{
+
+    double radius = 50;
+    String field = 'position';
+    var geo = Geoflutterfire();
+    var firestore = FirebaseFirestore.instance;
+    // Create a geoFirePoint
+    GeoFirePoint center ;
+    // get the collection reference or query
+    var collectionReference = firestore.collection(Category);
+    var pos = await _determinePosition();
+    Stream<List<DocumentSnapshot>> stream = geo.collection(collectionRef: collectionReference)
+        .within(center:  geo.point(latitude: pos.latitude, longitude:pos.longitude ), radius: radius, field: field);
+
+  }
+
+  static Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permantly denied, we cannot request permissions.');
+    }
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.whileInUse &&
+          permission != LocationPermission.always) {
+        return Future.error(
+            'Location permissions are denied (actual value: $permission).');
+      }
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+}
